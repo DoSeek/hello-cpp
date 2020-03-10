fc9a26aa93868d70d0823afdb215047d59ec1221
doseek <temparymail@qq.com>
2020-03-10 16:22:44 +0800

    update: git tag test v0.3.

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,7 @@ int main()
 	std::cout << "hello world" << std::endl;
 	std::cout << "hello c++ " << std::endl;
 	std::cout << "git tag test" << std::endl;
+	std::cout << "git tag v0.3" << std::endl;
 
 	return 0;
 }
